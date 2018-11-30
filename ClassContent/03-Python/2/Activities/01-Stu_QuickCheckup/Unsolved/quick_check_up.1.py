f5bd59c95d2c655bc8be74e3ef91d9e7e4ae91bc
Michael Sullivan <msullivan@planning.org>
2018-11-29 19:18:49 -0600

    added solutions

---
+++ ClassContent/03-Python/2/Activities/01-Stu_QuickCheckup/Unsolved/quick_check_up.1.py
@@ -0,0 +1,15 @@
+# Print Hello User!
+print('Hello User!')
+
+# Take in User Input
+getstr = str(input("Please enter something:"))
+
+# Respond Back with User Input
+print (getstr)
+
+# Take in the User Age
+getage = input("Please enter your age")
+
+# Respond Back with a statement based on age
+
+print("You are %s years old. That's experience" % getage)
